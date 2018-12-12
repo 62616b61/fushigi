c8824dfe088a56affc7be3bafb4fff8d8aa88c1b
whynotcrybot <alexanderkatsero@gmail.com>
2018-12-12 20:20:49 +0200

    add grpc server to controller

--- kubernetes-controller/src/index.js
+++ kubernetes-controller/src/index.js
@@ -1,3 +1,23 @@
-setInterval(() => {
-  console.log('Hello');
-}, 2500)
+var PROTO_PATH = __dirname + '/../proto/test.proto';
+
+var grpc = require('grpc');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(PROTO_PATH);
+var dataProto = grpc.loadPackageDefinition(packageDefinition).main;
+
+function getData(call, callback) {
+  callback(
+    null,
+    {
+      id: 501,
+      name: 'Alex',
+    },
+  );
+}
+
+var server = new grpc.Server();
+server.addService(dataProto.DataService.service, {getData: getData});
+server.bind('0.0.0.0:8080', grpc.ServerCredentials.createInsecure());
+server.start();
+
+console.log('grpc server is running');
