f3a0b01e1a93ffe7c29f64e92bcc4d54ecc44b42
whynotcrybot <alexanderkatsero@gmail.com>
2018-12-15 22:08:04 +0200

    spawn runner service on request

--- scheduler/src/libs/spawner.js
+++ scheduler/src/libs/spawner.js
@@ -3,9 +3,23 @@ const uuid = require('short-uuid');
 const k8s = require('kubernetes-client');
 
 const client = new k8s.Client({ config: k8s.config.getInCluster() });
-const job = yaml.load(__dirname + '/../../kubernetes/runner-job.yaml');
 
-function prepareRunnerJob(jobTemplate, id) {
+const jobTemplate = yaml.load(__dirname + '/../../kubernetes/runner-job.yaml');
+const serviceTemplate = yaml.load(__dirname + '/../../kubernetes/runner-service.yaml');
+
+function prepareRunnerService(id) {
+  const service = JSON.parse(JSON.stringify(serviceTemplate));
+
+  service.metadata.name += `-${id}`.toLowerCase();
+  service.metadata.labels.runner = id;
+  service.spec.selector.runner = id;
+
+  console.log('SERVICE MANIFEST', service);
+
+  return service;
+}
+
+function prepareRunnerJob(id) {
   const job = JSON.parse(JSON.stringify(jobTemplate));
 
   job.metadata.name += `-${id}`.toLowerCase();
@@ -19,8 +33,13 @@ async function spawnRunner() {
   await client.loadSpec();
 
   const id = uuid.generate();
-  const jobDefinition = prepareRunnerJob(job, id);
-  const result = await client.apis.batch.v1.namespaces('fushigi').jobs.post({ body: jobDefinition })
+  const jobDefinition = prepareRunnerJob(id);
+  const serviceDefinition = prepareRunnerService(id);
+
+  const result = Promise.all([
+    await client.apis.batch.v1.namespaces('fushigi').jobs.post({ body: jobDefinition }),
+    await client.apis.v1.namespaces('fushigi').services.post({ body: serviceDefinition }),
+  ]);
 }
 
 module.exports = {
