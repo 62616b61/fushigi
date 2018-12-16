ae3a73ce98d8ec6afb21ea08658141ea74e7aac0
whynotcrybot <alexanderkatsero@gmail.com>
2018-12-16 19:50:49 +0200

    add react client

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <header className="Home-header">
+          <p>
+            FUSHIGI
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Home;
