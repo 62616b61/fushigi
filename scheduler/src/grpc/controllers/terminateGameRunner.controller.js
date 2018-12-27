860bfafcd7e7520141e28d5f7c83481a53c3ac74
whynotcrybot <alexanderkatsero@gmail.com>
2018-12-27 21:54:43 +0200

    remove console log

--- scheduler/src/grpc/controllers/terminateGameRunner.controller.js
+++ scheduler/src/grpc/controllers/terminateGameRunner.controller.js
@@ -3,8 +3,6 @@ const { terminateRunner } = require('../../libs/kubernetes');
 async function terminateGameRunner(call, callback) {
   const { runner } = call.request;
 
-  console.log('REQUEST', call.request)
-
   callback(null, null);
 
   await terminateRunner(runner);
