59eed02b469af73e5c1bf84752ac90ed11d89913
whynotcrybot <alexanderkatsero@gmail.com>
2019-03-20 16:02:01 +0200

    refactor App component

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,20 @@ import { ContextConsumer } from './context';
 import Home from './pages/Home/Home'
 import Play from './pages/Play/Play'
 
-export default class App extends React.Component {
-  render () {
-    return (
-      <div>
-        <Route path="/" exact render={() => 
-          <ContextConsumer>
-            { (context) => <Home context={context} /> }
-          </ContextConsumer>
-        }/>
+export default function App() {
+  return (
+    <div>
+      <Route path="/" exact render={() => 
+        <ContextConsumer>
+          { (context) => <Home context={context} /> }
+        </ContextConsumer>
+      }/>
 
-        <Route path="/play" exact render={() => 
-          <ContextConsumer>
-            { (context) => <Play context={context} /> }
-          </ContextConsumer>
-        }/>
-      </div>
-    )
-  }
+      <Route path="/play" exact render={() => 
+        <ContextConsumer>
+          { (context) => <Play context={context} /> }
+        </ContextConsumer>
+      }/>
+    </div>
+  );
 }
-
--- client/src/index.js
+++ client/src/index.js
@@ -8,13 +8,12 @@ import App from './App';
 import 'semantic-ui-css/semantic.min.css';
 import './styles/index.css';
 
-
 const Root = () => (
   <ContextProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </ContextProvider>
-)
+);
 
 ReactDOM.render(<Root />, document.getElementById('root'));
