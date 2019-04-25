81357341d0594b9395780a6a1aeccfec173a5af5
whynotcrybot <alexanderkatsero@gmail.com>
2019-04-25 15:33:30 +0300

    modify Home page to support new useSocket and app context

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext, useReducer } from 'react';
 import { Button, Form, Grid, Segment, Dimmer, Loader } from 'semantic-ui-react';
-import { Redirect } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 import FushigiDefinition from '../../components/FushigiDefinition';
 import useSocket from '../../hooks/useSocket';
 
-import './Home.css';
+import { AppContext } from '../../context/app';
+
+import './home.module.css';
 
 const STEP_IDLE = 0;
 const STEP_LOOKING_FOR_OPPONENT = 1;
@@ -17,9 +19,59 @@ const MSG_TYPE_ASSIGNED_PLAYER_ID = 'assigned-player-id';
 const MSG_TYPE_OPPONENT_FOUND = 'opponent-found';
 const MSG_TYPE_RUNNER_READY = 'runner-ready';
 
-function Home ({ context }) {
+const initialHomeState = {
+  step: STEP_IDLE,
+  playerId: null,
+  runner: null,
+};
+
+function homeReducer (state, action) {
+  const { type, data } = action;
+
+  console.log('TPYPEP', type, data)
+
+  switch (type) {
+    case MSG_TYPE_JOIN: 
+      return {
+        ...state,
+        step: STEP_LOOKING_FOR_OPPONENT,
+      };
+
+    case MSG_TYPE_ASSIGNED_PLAYER_ID:
+      return {
+        ...state,
+        playerId: data.playerId,
+      };
+
+    case MSG_TYPE_OPPONENT_FOUND:
+      return {
+        ...state,
+        step: STEP_CREATING_RUNNER,
+      };
+
+    case MSG_TYPE_RUNNER_READY:
+      return {
+        ...state,
+        step: STEP_READY,
+        runner: data.runner,
+      };
+
+    default:
+      console.log('UNKNOWN ACTION TYPE');
+  }
+}
+
+const Home = withRouter(({ history }) => {
+  const {
+    savePlayerId,
+    saveNickname,
+    saveRunner,
+  } = useContext(AppContext);
+
   const [ nickname, setNickname ] = useState('');
-  const [ step, setStep ] = useState(STEP_IDLE);
+  const [ home, dispatch ] = useReducer(homeReducer, initialHomeState);
+
+  const { runner, playerId, step } = home;
 
   const { REACT_APP_RUNTIME, REACT_APP_HUB_URL } = process.env;
   const serverUrl = REACT_APP_RUNTIME === 'kubernetes' ? window.location.host : REACT_APP_HUB_URL;
@@ -28,27 +80,11 @@ function Home ({ context }) {
   const [isConnected, socket] = useSocket({
     name: 'Hub',
     url: socketUrl,
-    handlers: {
-      [MSG_TYPE_ASSIGNED_PLAYER_ID]: ({ data }) => {
-        context.savePlayerId(data.playerId);
-      },
-
-      [MSG_TYPE_OPPONENT_FOUND]: ({ data }) => {
-        context.saveOpponentNickname(data.opponentNickname);
-        setStep(STEP_CREATING_RUNNER);
-      },
-
-      [MSG_TYPE_RUNNER_READY]: ({ data }) => {
-        context.saveRunner(data.runner);
-        setStep(STEP_READY);
-      },
-    },
+    dispatch,
   });
 
   const sendJoinMessage = () => {
-    context.saveNickname(nickname);
-
-    setStep(STEP_LOOKING_FOR_OPPONENT)
+    dispatch({ type: MSG_TYPE_JOIN })
 
     const message = JSON.stringify({
       type: MSG_TYPE_JOIN,
@@ -58,9 +94,26 @@ function Home ({ context }) {
     socket.send(message);
   };
 
+  useEffect(() => {
+    if (step === STEP_READY) {
+      saveNickname(nickname);
+      savePlayerId(playerId);
+      saveRunner(runner);
+
+      history.push('/play/');
+    }
+  }, [step]);
+
+  useEffect(() => {
+    // automatically join a game if running in docker-compose
+    if (REACT_APP_RUNTIME !== 'kubernetes' && isConnected) {
+      setNickname('kek?');
+      sendJoinMessage();
+    };
+  }, [isConnected]);
+
   return (
     <div>
-      {step === STEP_READY ? <Redirect push to="/play" /> : null}
       <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
         <Grid.Column width={4}>
           <FushigiDefinition />
@@ -102,6 +155,6 @@ function Home ({ context }) {
       </Grid>
     </div>
   );
-}
+});
 
 export default Home;
